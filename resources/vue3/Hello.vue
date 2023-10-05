c031c132e601be11ad56b788785dddc0bca162a7
issei miyara <issei_miyara@rebuild-inc.co.jp>
2023-10-05 20:16:17 +0900

    inertiaの初期設定をしてvue3が使えるようになった

---
+++ resources/vue3/Hello.vue
@@ -0,0 +1,8 @@
+<template>
+    <h1>This is Vue3!!!!</h1>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+export default defineComponent({});
+</script>
--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/vue', function () {
+    return \Inertia\Inertia::render('Hello');
 });

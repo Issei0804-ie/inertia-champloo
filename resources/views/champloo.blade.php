7255c44c0c3e75ef63c5e3bf2b124f02f7480d88
issei miyara <issei_miyara@rebuild-inc.co.jp>
2023-10-07 00:14:40 +0900

    cham

--- resources/views/champloo.blade.php
+++ resources/views/champloo.blade.php
@@ -4,6 +4,8 @@
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0" />
     @vite('resources/js/vue3.js')
+    @vite('resources/js/react.jsx')
+    @vite('resources/js/svelte.js')
     @inertiaHead
 </head>
 <body>
